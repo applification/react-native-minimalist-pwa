ec531f4c2e21a38f6e8b4c2036a732bee89f230e
Dave Hudson <dave@applificationhq.com>
2018-04-17 13:58:12 +0100

    Added manifest.json for PWA support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,34 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'React Native Minimalist PWA',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-react-next',
     'gatsby-plugin-react-native-web',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'PWA',
+        short_name: 'PWA',
+        start_url: '/',
+        background_color: '#f7f0eb',
+        theme_color: '#a2466c',
+        display: 'minimal-ui',
+        icon: 'icon.png',
+        icons: [
+          {
+            src: `/favicons/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
   ],
 }
